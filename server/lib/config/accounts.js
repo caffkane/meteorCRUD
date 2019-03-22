b934a9493681a42e0b9fbbd3c28412e43ba8cd4c
Logan Kane <logan.kane1@gmail.com>
2019-03-22 12:39:12 -0700

    Update accounts.js

--- server/lib/config/accounts.js
+++ server/lib/config/accounts.js
@@ -13,8 +13,8 @@ Meteor.startup(function() {
     ServiceConfiguration.configurations.update(
     { service: "twitter" },
     { $set: {
-        appId: "XXXXXXXXXXXXXXX",
-        secret: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        appId: "16152429",
+        secret: "L8edt7eEH15Z5Fa6NQRsC7ils9PgOZQ7M9jsXwUVrw4jD5VGYk"
       }
     },
     { upsert: true }
@@ -25,9 +25,9 @@ Meteor.startup(function() {
   ServiceConfiguration.configurations.update(
     { service: "google" },
     { $set: {
-        clientId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        client_email: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        secret: "XXXXXXXXXXXXXXXXXXXXXXXX"
+        clientId: "1018883713702-flios1jr1bgj5cheup3gm5qgq85107u4.apps.googleusercontent.com",
+        client_email: "logan.kane1@gmail.com",
+        secret: "cnKM6jAytV1Qa9zZ3LOBXTrq"
       }
     },
     { upsert: true }
